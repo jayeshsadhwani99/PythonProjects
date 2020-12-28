9912cb575cc301d1c1fcde27991356f851ea1e79
jayeshsadhwani99 <69295377+jayeshsadhwani99@users.noreply.github.com>
2020-12-28 11:17:18 +0530

    Communication of client and server

--- ONLINEMULTIPLAYER/network.py
+++ ONLINEMULTIPLAYER/network.py
@@ -6,8 +6,10 @@ class Network:
         self.server = "192.168.0.105"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.pos = self.connect()
+
+    def getPos(self):
+        return self.pos
 
     def connect(self):
         try:
@@ -22,7 +24,3 @@ class Network:
             return self.client.recv(2048).decode()
         except socket.error as e:
             print(e)
-
-n =  Network()
-print(n.send("Hello"))
-print(n.send("Working"))
